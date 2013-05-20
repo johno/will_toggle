12ca4d56b5d5e097bb91caee95a1dd188ea3be39
johnotander <johnotander@gmail.com>
2013-05-20 17:03:11 -0600

    Update railtie.

--- lib/will_toggle/railtie.rb
+++ lib/will_toggle/railtie.rb
@@ -4,8 +4,7 @@ module WillToggle
   class Railtie < Rails::Railtie
     initializer "will_toggle" do |app|
       ActionView::Base.send :include, WillToggle
+      require 'view_helpers'
     end
-    
-    require 'view_helpers'
   end
 end
