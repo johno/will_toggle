e182d7246027b3dd5ca1e4b6f64b8fd6f9ac764f
Ben Neely <benjaminneely@u.boisestate.edu>
2013-08-16 10:32:39 -0600

    Add radio button functionality to will-toggle

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -8,11 +8,30 @@ module WillToggle
       generate_html(attribute, options).html_safe
     end
   
+    def will_toggle_radio(name, options = {})
+      @@toggle_index += 1
+      generate_radio_html(name, options)
+    end
+
     def generate_html(attribute, options = {})
       <<-HTML
         <div class='will-toggle-wrapper'>
           <div class='field check-box'>
             #{ get_check_box(attribute, options) }
+          </div>
+          <div class='will-toggle-content' id="will-toggle-#{ @@toggle_index }" style="display: #{ visibility(options[:checked]) };">
+            #{ get_partial(options) }
+          </div>
+        </div>
+      HTML
+    end
+
+
+    def generate_html(attribute, options = {})
+      <<-HTML
+        <div class='will-toggle-wrapper'>
+          <div class='field radio-button'>
+            #{ get_radio_button(attribute, options) }
           </div>
           <div class='will-toggle-content' id="will-toggle-#{ @@toggle_index }" style="display: #{ visibility(options[:checked]) };">
             #{ get_partial(options) }
@@ -36,6 +55,19 @@ module WillToggle
       end
       html
     end
+
+    def get_radio_button(name, options = {})
+      html = ''
+      if name
+        html << options[:form].radio_button(name, onChange: js_call(options), class: 'check-box will-toggle-radio-button')
+        html << options[:form].label(attribute, options[:label])
+      else
+        html << radio_button_tag(name, nil, options[:checked], onChange: js_call(options), class: 'check-box will-toggle-radio-button')
+        html << label_tag(nil, options[:label], class: 'will-toggle-label')
+      end
+      html
+    end
+
     
     def get_partial(options = {})
       render partial: options[:partial], 
